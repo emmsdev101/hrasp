1dce8a5c3fd02fc04952f131697ca19b696dc09d
emmsdev101 <emman199810@example.com>
2023-02-01 15:12:31 +0800

    For update

--- src/config.js
+++ src/config.js
@@ -1,6 +1,11 @@
 //exports.apiBaseUrl = "http://18.191.54.215:4000" 
 //exports.apiBaseUrl = "http://192.168.254.137:4000"
-exports.apiBaseUrl = "https://ahp-api.onrender.com:4000"
-exports.peerServer = "ahp-api.onrender.com"
+//exports.apiBaseUrl = "https://ahp-api.onrender.com:4000"
+
+exports.apiBaseUrl = "http://192.168.254.179:4000"
+
+exports.peerServer = "192.168.254.179"
 exports.peerServerPort = "4001"
-exports.webSocketServer = "ahp-api.onrender.com" 
+exports.webSocketServer = "192.168.254.179:3001" 
+
+// chrome://flags/#unsafely-treat-insecure-origin-as-secure
