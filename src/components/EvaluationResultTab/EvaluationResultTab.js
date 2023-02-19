e15e2a661ab309248bd6cef2f9b9f49e37148fcc
EmmsDevs <emman199810@gmail.com>
2023-02-20 00:50:53 +0800

    Fixed evaluaton result

--- src/components/EvaluationResultTab/EvaluationResultTab.js
+++ src/components/EvaluationResultTab/EvaluationResultTab.js
@@ -1,6 +1,8 @@
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Card, Col, Form, Row } from "react-bootstrap";
+import { Button, Card, Col, Container, Form, NavLink, Row } from "react-bootstrap";
 import { apiBaseUrl } from "../../config";
 import ApplicationsTableHeader from "../ApplicantionsTableHeader/ApplicationsTableHeader";
 import EvaluationTable from "../EvaluationTable/EvaluationTable";
@@ -9,6 +11,7 @@ import "./evaluation.css";
 
 export default function EvaluationResultTab({ panel, head, committee }) {
   const [evaluations, setEvaluations] = useState([]);
+  const [detailedEvaluation, setDetailedEvaluation] = useState([])
   const [viewDetails, setviewDetails] = useState(false)
 
   
@@ -25,14 +28,45 @@ export default function EvaluationResultTab({ panel, head, committee }) {
     });
     const evaluationData = evaluationRequest.data;
     if (evaluationData) {
-      console.log(evaluationData);
       setEvaluations(evaluationData);
     }
   };
   useEffect(() => {
     fetchEvaluations();
   }, []);
-  return (
+
+  useEffect(()=>{
+    if(viewDetails){
+      getPanelEvaluations()
+    }
+  }, [viewDetails])
+
+  const getPanelEvaluations = async()=>{
+    const reqUrl = apiBaseUrl + "/admin/getPanelEvaluations/"+viewDetails.id;
+  const evaluationRequest = await axios.get(reqUrl, {
+    withCredentials: true,
+  });
+  const evaluationData = evaluationRequest.data;
+  if (evaluationData) {
+    setDetailedEvaluation(evaluationData);
+  }
+  }
+
+  const viewEvaluation = (data) => {
+    window.location.href = "../view-evaluation/"+data.id
+  }
+  const viewEvaluationResult = (data) =>{
+    setviewDetails(data)
+  }
+  return viewDetails ?(<ViewResult data = {viewDetails} evaluations = {detailedEvaluation}/>):(
+    <Result evaluations={evaluations} panel = {panel} viewEvaluation = {viewEvaluation} viewEvaluationResult = {viewEvaluationResult}/>
+  )
+}
+  
+
+
+const Result = ({evaluations, panel,viewEvaluation, viewEvaluationResult }) => {
+  return(
     <Row className="w-100 evalduation">
       <Col>
         <Card>
@@ -76,10 +110,49 @@ export default function EvaluationResultTab({ panel, head, committee }) {
             </Row>
           </Card.Header>
           <Card.Body className="p-3">
-            <EvaluationTable data={evaluations} panel = {panel}/>
+            <EvaluationTable action = {panel?viewEvaluation:viewEvaluationResult} data={evaluations} panel = {panel}/>
           </Card.Body>
         </Card>
       </Col>
     </Row>
-  );
+  )
+}
+
+const ViewResult = ({data, evaluations}) =>{
+  return (
+  <Row className="w-100 evalduation">
+      <Col>
+        <Card>
+          <Card.Header className="p-3 m-3">
+            <Container fluid className="mb-2 p-0 d-flex justify-content-start alert-items-center">
+            <FontAwesomeIcon icon={faArrowLeft} size="lg" onClick={()=>window.location.href = "../applications/evaluation-result"}/>
+
+            <p className="p-2 m-0 me-2 alert alert-info">Applying: {data.title}</p>
+                  <p className="p-2 m-0 me-2 alert alert-info">For: {data.department}</p>
+            </Container>
+            <Row>
+              <Col>
+                <div className="d-flex justify-content-between align-items-center">
+                  <h5 className="p-0 m-0">{data.applicant_name}</h5>
+                </div>
+              </Col>
+              <Col className="d-flex justify-content-end align-items-center">
+                <Filter
+                  title="Filter"
+                  list={["To be considered"]}
+                  id="recommendation"
+                  defaultValue="SICT"
+                  handler={null}
+                />
+                <p className="alert alert-danger m-0 p-2 w-auto">Total: {data.total}</p>
+              </Col>
+            </Row>
+          </Card.Header>
+          <Card.Body className="p-3">
+            <EvaluationTable details={true} action = {null} data={evaluations} panel = {false}/>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  )
 }
--- src/components/EvaluationTable/EvaluationTable.js
+++ src/components/EvaluationTable/EvaluationTable.js
@@ -1,46 +1,82 @@
-import React from 'react'
-import { Table } from 'react-bootstrap'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Table } from "react-bootstrap";
+import { apiBaseUrl } from "../../config";
 
-export default function EvaluationTable({data,panel, details}) {
+export default function EvaluationTable({ data, panel, details, action }) {
+  
   return (
     <Table hover responsive>
-        <thead>
-            {details?(
-                <tr>
+      <thead>
+        {details ? (
+          <tr>
             <th>Evaluator</th>
             <th>Recommendationn</th>
             <th>Remarks</th>
             <th>Score</th>
-            </tr>
-            ):
-            (
-                <tr>
+          </tr>
+        ) : (
+          <tr>
             <th>Rank</th>
             <th>Applicant Name</th>
             <th>Position</th>
             <th>Department</th>
             <th>Department Type</th>
-            {panel?(<th>Recommendationn</th>):""}
-            {panel?(<th>Remarks</th>):""}
+            {panel ? <th>Recommendationn</th> : ""}
+            {panel ? <th>Remarks</th> : ""}
             <th>Score</th>
-            </tr>
-            )}
-        </thead>
-        <tbody>
-            {data.map((values, idx)=>(
-                <tr key={idx} onClick = {()=>window.location.href = "../view-evaluation/"+values.id}>
-                    <th>{idx+1}</th>
-                    <td>{values.applicant_name}</td>
-                    <td>{values.title}</td>
-                    <td>{values.department}</td>
-                    <td>{values.departmentType}</td>
-                    {panel?( <td>{values.recommendation}</td>):""}
-                    {panel? (<td>{values.remarks}</td>):""}
-                    <td>{values.total}</td>
-                </tr>
+          </tr>
+        )}
+      </thead>
+      <tbody>
+        {details
+          ? data.map((values, idx) => (
+              <DetailsItem data={values} key = {idx}/>
+            ))
+          : data.map((values, idx) => (
+              <tr key={idx} onClick={() => action(values)}>
+                <th>{idx + 1}</th>
+                <td>{values.applicant_name}</td>
+                <td>{values.title}</td>
+                <td>{values.department}</td>
+                <td>{values.departmentType}</td>
+                {panel ? <td>{values.recommendation}</td> : ""}
+                {panel ? <td>{values.remarks}</td> : ""}
+                <td>{values.total}</td>
+              </tr>
             ))}
-           
-        </tbody>
+      </tbody>
     </Table>
-  )
+  );
+}
+const DetailsItem = ({data}) =>{
+    const [evaluator, setEvaluator] = useState(null) 
+
+    useEffect(()=>{
+        fetchCommitteeDetails()
+    },[])
+    const fetchCommitteeDetails = async () => {
+        const ispanel = data.type === "panel";
+        console.log(ispanel)
+    
+        let url = apiBaseUrl + "/admin/getCommitteeDetails/" + data.evaluator;
+    
+        if (ispanel) {
+          url = apiBaseUrl + "/admin/getPanelDetails/" + data.evaluator;
+        }
+        const request = await axios.get(url, { withCredentials: true });
+    
+        const requestData = request.data
+        console.log(data)
+        setEvaluator(requestData)
+        
+      };
+      return (
+        <tr onClick={() => window.location.href = "../view-evaluation/"+data.id}>
+                <td>{evaluator?.fullname}</td>
+                <td>{data.recommendation}</td>
+                <td>{data.remarks}</td>
+                <td>{data.total}</td>
+              </tr>
+      )
 }
